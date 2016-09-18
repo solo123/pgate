1cc60bd33404c15db1ca1144d353c45ba97964b3
jimmy <solo123@21cn.com>
2016-09-19 02:00:01 +0800

    payment

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -1,14 +1,24 @@
 class PaymentController < ApplicationController
   def pay
+    biz = Biz::GatewayPaymentBiz.new
+    js = biz.check_required_params(params)
+    if js
+      render json: js
+      return
+    end
 
-    js = nil
-    if %W(org_id trans_type mac).any? {|fld| params[fld].nil? }
-      js = {resp_code: '30', resp_desc: '报文错，缺少org_id或trans_type或mac'}
-    else
-      client = Client.find_by(org_id: params[:org_id])
-      if client.nil?
-        js = {resp_code: '03', resp_desc: "无此商户: #{params[:org_id]}", status: 7}
-      else
+    payment = ClientPayment.new(params)
+    js = payment.check_payment_fields
+    payment.reload
+    if js[:resp_code] == '00'
+      biz = Biz::KaiFuApi.new
+      biz.send_kaifu_payment(payment)
+    end
+    payment.reload
+    render json: {resp_code: payment.resp_code, resp_desc: payment.resp_desc, redirect_url: payment.redirect_url}
+  end
+
+  def validate_params(params)
         case params[:trans_type]
         when 'P001'
           js = do_p001(client, params)
@@ -31,6 +41,18 @@ class PaymentController < ApplicationController
       payment.fee = payment.amount * client.d0_min_percent / 1000000 + client.d0_min_fee
     end
     payment.save
+    mab = ''
+    fields.sort.each do |fld|
+      if fld != 'mac'
+        mab << params[fld]
+      end
+    end
+    mac = Digest::MD5.hexdigest(mab + client.tmk).upcase
+    if payment.mac.upcase != mac
+      js = {resp_code: 'A0', resp_code: '检验mac错', status: 7}
+      payment.update(js)
+      return js
+    end
 
     biz = Biz::KaifuApi.new
     b001_gw = biz.create_b001(payment)
