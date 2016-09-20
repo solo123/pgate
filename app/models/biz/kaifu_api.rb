5c9338b0ac2233a766fa11eab262d75d6b0b4aa6
jimmy <solo123@21cn.com>
2016-09-21 02:44:54 +0800

    b002

--- app/models/biz/kaifu_api.rb
+++ app/models/biz/kaifu_api.rb
@@ -8,8 +8,11 @@ module Biz
     OPENID_B001_FLDS = "sendTime,sendSeqId,transType,organizationId,payPass,transAmt,fee,cardNo,name,idNum,body,notifyUrl,callbackUrl"
 
     def send_kaifu_payment(client_payment)
-      if client_payment.trans_type == 'P001'
+      case client_payment.trans_type
+      when 'P001'
         create_b001(client_payment)
+      when 'P002'
+        create_b002(client_payment)
       else
         {resp_code: '12', resp_desc: "无此交易：#{client_payment.trans_type}"}
       end
@@ -30,6 +33,24 @@ module Biz
         notify_url: NOTIFY_URL,
         callback_url: client_payment.callback_url
       }
+      return create_kaifu_payment(client_payment, js)
+    end
+    def create_b002(client_payment)
+      js = {
+        send_time: Time.now.strftime("%Y%m%d%H%M%S"),
+        send_seq_id: "P2" + ('%06d' % client_payment.id),
+        trans_type: 'B002',
+        organization_id: ORG_ID,
+        pay_pass: client_payment.pay_pass,
+        trans_amt: client_payment.amount.to_s,
+        fee: client_payment.fee.to_s,
+        body: "#{client_payment.client.name} - #{client_payment.order_title}",
+        notify_url: NOTIFY_URL,
+        callback_url: client_payment.callback_url
+      }
+      return create_kaifu_payment(client_payment, js)
+    end
+    def create_kaifu_payment(client_payment, js)
       kf_js = kaifu_api_format(js)
       js[:mac] = kf_js["mac"] = get_mac(kf_js, TMK)
       gw = KaifuGateway.new(js)
