8ce3b186c8f8a3c0de36f451227c3ef5fdda5697
jimmy <solo123@21cn.com>
2016-10-12 12:11:30 +0800

    remote ip

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -12,7 +12,7 @@ class PaymentController < ApplicationController
       render json: {resp_code: '03', resp_desc: "无此客户:#{js[:org_id]}"}
       return
     end
-    payment.remote_ip = request.headers['remote-addr']
+    payment.remote_ip = request.remote_ip if payment.remote_ip.empty?
     payment.uni_order_id = "#{payment.org_id}-#{payment.order_time[0..7]}-#{payment.order_id}"
     payment.save
 
