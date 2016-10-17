0e5e6440a6522e3da4132e3f13c09ad70f356f49
jimmy <solo123@21cn.com>
2016-10-18 02:22:27 +0800

    pay

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -1,7 +1,10 @@
 class PaymentController < ApplicationController
   def pay
+    log = BizLog.create(op_name: 'payment', op_message: params.inspect)
     js = Biz::PaymentBiz.parse_data_json(params[:data])
     if js[:resp_code] != '00'
+      log.op_result = js.to_s
+      log.save
       render json: js
       return
     end
@@ -9,14 +12,20 @@ class PaymentController < ApplicationController
     payment = ClientPayment.new
     Biz::PaymentBiz.update_json(payment, js)
     unless payment.client = Client.find_by(org_id: js[:org_id])
-      render json: {resp_code: '03', resp_desc: "无此客户:#{js[:org_id]}"}
+      js = {resp_code: '03', resp_desc: "无此客户:#{js[:org_id]}"}
+      log.op_result = js.to_s
+      log.save
+      render json: js
       return
     end
     payment.remote_ip = request.remote_ip if payment.remote_ip
     payment.uni_order_id = "#{payment.org_id}-#{payment.order_time[0..7]}-#{payment.order_id}"
     payment.save
 
-    render json: Biz::PooulApi.payment(payment)
+    js = Biz::PooulApi.payment(payment)
+    log.op_result = js.to_s
+    log.save
+    render json: js
   end
 
 end
